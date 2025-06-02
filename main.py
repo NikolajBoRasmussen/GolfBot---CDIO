1a798fdfa9630966eb7388847920e3b6fdfb2a32
NikolajBoRasmussen <s235098@dtu.dk>
2025-06-02 17:09:24 +0200

    Fixed - det er kameraets maksimale resolution, der bliver brugt nu

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description= "YOLOv8 live")
     parser.add_argument(
         "--webcam-resolution",
-        default= [640, 640],
+        default= [1280 , 720],
         nargs = 2,
         type=int
     )
