5df97752144f034b5ed579264ba3b0babe04b9bb
NikolajBoRasmussen <s235098@dtu.dk>
2025-06-02 17:01:01 +0200

    Forsøg på at ændre resolution

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
 from ultralytics import YOLO
-import torch
-import os
-import shutil
-import random
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import pandas as pd
-import json
-import time
-import copy
-import glob
-import sys
 import cv2
+import argparse
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description= "YOLOv8 live")
+    parser.add_argument(
+        "--webcam-resolution",
+        default= [640, 640],
+        nargs = 2,
+        type=int
+    )
+    args = parser.parse_args()
+    return args
 
 def main():
+    args = parse_arguments()
+    frame_width, frame_height = args.webcam_resolution
+    
     #print(cv2.getBuildInformation())
     #nedenstående funktion tænder kameraet i index 1. Indexet starter på 0, men for mig der er kameraet i index 0 mit kamera i min pc.
     cap = cv2.VideoCapture(1)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+    
+    #model = YOLO("yolo8l.pt")
+    
     
     while True:
         ret, frame = cap.read()
         cv2.imshow("yolov8", frame)
         
+        print(frame.shape)
         #tryk escape for at stoppe programmet
         if(cv2.waitKey(30)==27):
             break
